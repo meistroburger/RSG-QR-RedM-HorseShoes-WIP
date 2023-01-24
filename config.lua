ee9f18c3255c9c3b0879b5d5252119c436634bd0
Uncle Zeke <117952305+MDGamesmaster@users.noreply.github.com>
2023-01-24 22:56:30 +0000

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,13 @@
+-- Chance of losing a shoe at different speeds
+speed1Chance = 0
+speed2Chance = 0.1
+speed3Chance = 0.2
+speed4Chance = 0.3
+speed5Chance = 0.4
+
+-- Number of shoes that can be lost at different speeds
+speed1MaxLoss = 0
+speed2MaxLoss = 1
+speed3MaxLoss = 2
+speed4MaxLoss = 3
+speed5MaxLoss = 4
